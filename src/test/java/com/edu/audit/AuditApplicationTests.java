e6612164ce1997258067ee425c7b42c049580cf2
Vince <527687529@qq.com>
2020-02-04 17:36:02 +0800

    工程跟踪审计管理系统开发

---
+++ src/test/java/com/edu/audit/AuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
