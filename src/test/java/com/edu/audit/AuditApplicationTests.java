660cbe6df2e60ad855aaef0ac951231cbaed47ec
Vince <527687529@qq.com>
2020-02-15 18:44:52 +0800

    feat: 添加发送邮箱基础功能

--- src/test/java/com/edu/audit/AuditApplicationTests.java
+++ src/test/java/com/edu/audit/AuditApplicationTests.java
@@ -3,14 +3,27 @@ package com.edu.audit;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class AuditApplicationTests {
+    @Autowired
+    JavaMailSenderImpl javaMailSender;
 
     @Test
     void contextLoads() {
-
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("527687529@qq.com");
+        message.setTo("350660213@qq.com");
+        message.setSubject("测试");
+        message.setText("邮箱测试");
+        javaMailSender.send(message);
     }
 
 }
